2c7f8dce3253cff03aa2aa7354d6618e1b0c2c3f
cauaMascena <cauasinho656@gmail.com>
2021-08-24 09:11:57 -0300

    acabou eu acho

---
+++ lampada.js
@@ -0,0 +1,33 @@
+"use strict"
+
+function ligarLampada() {
+    const lampada = document.getElementById("lampada")
+    const ligar = document.getElementById("ligar")
+    const desligar = document.getElementById("desligar")
+
+    lampada.src = "img/ligada.jpg"
+    ligar.disabled = true
+    deligar.disabled = false 
+}
+function desligarLampada() {
+    lampada.src = document.getElementById("lampada")
+    lampada.src = "img/desligada.jpg"
+
+    lampada.src = "img/desligada.jpg"
+    ligar.disabled = false
+    deligar.disabled = true 
+}
+function quebrarLampada() {
+    lampada.src = document.getElementById("lampada")
+    lampada.src = "img/quebrada.jpg"
+}
+
+//  Eventos
+document.getElementById("ligar")
+.addEventListener("click", ligarLampada)
+
+document.getElementById("desligar")
+.addEventListener("click", desligarLampada)
+
+document.getElementById("lampada")
+.addEventListener("mousemove", quebrarLampada)
